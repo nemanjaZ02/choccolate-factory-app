e8f1ed12cc964641c3dfe9e98fb0d1616fc6e2bb
nemanajZ02 <nemanja.zekanovic002@gmail.com>
2024-07-02 20:47:58 +0200

    Added suspicious users
    - Users who canceled their orders in period of one month are now standing out with their color

--- Backend/ChoccolateAppREST/src/beans/Manager.java
+++ Backend/ChoccolateAppREST/src/beans/Manager.java
@@ -15,12 +15,12 @@ public class Manager extends User {
 	}
 
 	public Manager(int id, String username, String password, String name, String surname, Gender gender, Date birthday,
-			Role role, boolean isDeleted) {
-		super(id, username, password, name, surname, gender, birthday, role, isDeleted);
+			Role role, boolean isDeleted, boolean isSuspicious, boolean isBanned) {
+		super(id, username, password, name, surname, gender, birthday, role, isDeleted, isSuspicious, isBanned);
 		// TODO Auto-generated constructor stub
 	}
 	public Manager(User user) {
-		super(user.getId(),user.getUsername(),user.getPassword(),user.getName(),user.getSurname(),user.getGender(),user.getBirthday(),user.getRole(), user.getIsDeleted());
+		super(user.getId(),user.getUsername(),user.getPassword(),user.getName(),user.getSurname(),user.getGender(),user.getBirthday(),user.getRole(), user.getIsDeleted(),user.getIsSuspicious(),user.getIsBanned());
 		// TODO Auto-generated constructor stub
 	}
 
